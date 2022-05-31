9e0a29e37504b64d3392a760dd1ad4d74dcdc34a
tunalad <novica.arsic@aol.com>
2022-05-31 18:19:51 +0200

    PDF splitting functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from os import system, mkdir, path
+from pprint import pprint
 
+from PyPDF2 import PdfFileReader, PdfFileWriter
 from pdf2image import convert_from_path
 
 
@@ -17,10 +19,48 @@ def pdf_to_img(pdf_path):
         icon.save(f"tempfiles/{icon}-out.jpeg", "JPEG")
 
 
+def pdf_extract_page(all_pages, page):
+    pdf_writer = PdfFileWriter()
+
+    pdf_writer.addPage(all_pages[page])
+
+    pdf_out = open(f"page-{page}-out.pdf", "wb")
+    pdf_writer.write(pdf_out)
+
+    pdf_out.close()
+
+
+def pdf_extract_array(all_pages, new_pages):
+    pdf_writer = PdfFileWriter()
+
+    for i in new_pages:
+        pdf_writer.addPage(all_pages[i])
+
+    pdf_out = open(f"{len(new_pages)}-pages-out.pdf", "wb")
+    pdf_writer.write(pdf_out)
+    pdf_out.close()
+
+
 if __name__ == "__main__":
     pdf_path = "Blender 2.9 Shortcuts v1.1.pdf"
+    pdf_in = open(pdf_path, "rb")
+    pdf_reader = PdfFileReader(pdf_in)
 
-    temp_dir()
-    pdf_to_img(pdf_path)
+    # temp_dir()
+    # pdf_to_img(pdf_path)
+
+    # add pages to an array
+    pdf_pages = []
+    for page in range(pdf_reader.numPages):
+        pdf_pages.append(pdf_reader.getPage(page))
+
+    # page
+    pdf_extract_page(pdf_pages, 0)
+
+    # pages
+    pdf_selected = [0, 2, 4]
+    pdf_extract_array(pdf_pages, pdf_selected)
 
 
+    pdf_in.close()
+    system("notify-send 'process finished with exit code 0'")
