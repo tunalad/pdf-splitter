9ae6eb4332dfe3b1ecdbfbc89b4a49ff675ca315
tunalad <novica.arsic@aol.com>
2022-05-31 19:35:23 +0200

    Moved pdf handling to a class

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 from os import system, mkdir, path
-from pprint import pprint
 
-from PyPDF2 import PdfFileReader, PdfFileWriter
-from pdf2image import convert_from_path
+import pdf_handler
 
 
 def temp_dir():
@@ -13,54 +11,14 @@ def temp_dir():
         print(e)
 
 
-def pdf_to_img(pdf_path):
-    pdf_icons = convert_from_path(pdf_path, 50)
-    for icon in pdf_icons:
-        icon.save(f"tempfiles/{icon}-out.jpeg", "JPEG")
-
-
-def pdf_extract_page(all_pages, page):
-    pdf_writer = PdfFileWriter()
-
-    pdf_writer.addPage(all_pages[page])
-
-    pdf_out = open(f"page-{page}-out.pdf", "wb")
-    pdf_writer.write(pdf_out)
-
-    pdf_out.close()
-
-
-def pdf_extract_array(all_pages, new_pages):
-    pdf_writer = PdfFileWriter()
-
-    for i in new_pages:
-        pdf_writer.addPage(all_pages[i])
-
-    pdf_out = open(f"{len(new_pages)}-pages-out.pdf", "wb")
-    pdf_writer.write(pdf_out)
-    pdf_out.close()
-
-
 if __name__ == "__main__":
     pdf_path = "Blender 2.9 Shortcuts v1.1.pdf"
-    pdf_in = open(pdf_path, "rb")
-    pdf_reader = PdfFileReader(pdf_in)
 
-    # temp_dir()
-    # pdf_to_img(pdf_path)
+    pdf = pdf_handler.file(pdf_path)
+    pages = pdf.get_pages()
 
-    # add pages to an array
-    pdf_pages = []
-    for page in range(pdf_reader.numPages):
-        pdf_pages.append(pdf_reader.getPage(page))
+    pdf.to_images()
+    pdf.extract_page(pages, 0)
+    pdf.extract_array(pages, [0, 2, 4])
 
-    # page
-    pdf_extract_page(pdf_pages, 0)
-
-    # pages
-    pdf_selected = [0, 2, 4]
-    pdf_extract_array(pdf_pages, pdf_selected)
-
-
-    pdf_in.close()
     system("notify-send 'process finished with exit code 0'")
---
+++ pdf_handler.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from pdf2image import convert_from_path
+
+
+class file():
+    def __init__(self, pdf_path):
+        self.pdf_path = pdf_path
+
+        # self.pdf_path = "Blender 2.9 Shortcuts v1.1.pdf"
+
+        self.pdf_in = open(pdf_path, "rb")
+        self.pdf_reader = PdfFileReader(self.pdf_in)
+
+    def get_pages(self):
+        pdf_pages = []
+        for page in range(self.pdf_reader.numPages):
+            pdf_pages.append(self.pdf_reader.getPage(page))
+        return pdf_pages
+
+    def to_images(self):
+        pdf_icons = convert_from_path(self.pdf_path, 50)
+        for icon in pdf_icons:
+            icon.save(f"tempfiles/{icon}-out.jpeg", "JPEG")
+
+    def extract_page(self, all_pages, page):
+        pdf_writer = PdfFileWriter()
+
+        pdf_writer.addPage(all_pages[page])
+
+        pdf_out = open(f"page-{page}-out.pdf", "wb")
+        pdf_writer.write(pdf_out)
+
+        pdf_out.close()
+
+    def extract_array(self, all_pages, new_pages):
+        pdf_writer = PdfFileWriter()
+
+        for i in new_pages:
+            pdf_writer.addPage(all_pages[i])
+
+        pdf_out = open(f"{len(new_pages)}-pages-out.pdf", "wb")
+        pdf_writer.write(pdf_out)
+        pdf_out.close()
