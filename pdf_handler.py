30f1288df1dbb03b0daa9a6e892e9dff8be70d72
tunalad <28594843+tunalad@users.noreply.github.com>
2023-10-28 23:39:32 +0200

    returned PyPDF, so MuPDF only does the icons part
    replacing everything with MuPDF broke everything, and I didn't test
    before pushing the code. Will move to MuPDF only later down the line
    (maybe)

--- pdf_handler.py
+++ pdf_handler.py
@@ -1,44 +1,44 @@
 from tempfile import TemporaryDirectory, gettempdir
 from os import path
-
+from PyPDF2 import PdfFileReader, PdfFileWriter
 import fitz
 
 
 class file:
     def __init__(self, pdf_path):
         self.pdf_path = pdf_path
-        self.pdf_document = fitz.open(self.pdf_path)
+        self.pdf_in = open(pdf_path, "rb")
+        self.pdf_reader = PdfFileReader(self.pdf_in)
 
     def get_pages(self):
         pdf_pages = []
-        for page_num in range(self.pdf_document.page_count):
-            page = self.pdf_document.load_page(page_num)
-            pdf_pages.append(page)
+        for page in range(self.pdf_reader.numPages):
+            pdf_pages.append(self.pdf_reader.getPage(page))
         return pdf_pages
 
     def to_images(self, resolution=300):
-        for page_num in range(self.pdf_document.page_count):
-            page = self.pdf_document.load_page(page_num)
+        pdf_document = fitz.open(self.pdf_path)
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document.load_page(page_num)
             image = page.get_pixmap(
                 matrix=fitz.Matrix(resolution / 72, resolution / 72)
             )
             image.save(f"{gettempdir()}/{page_num}.jpeg")
 
-    def extract_page(self, page):
+    def extract_page(self, all_pages, page):
         file_name = path.basename(self.pdf_path).replace(".pdf", "")
-        pdf_writer = fitz.open()
-        pdf_writer.insert_pdf(self.pdf_document, from_page=page, to_page=page)
-        pdf_writer.save(f"{file_name}-page-{page}-out.pdf")
-        pdf_writer.close()
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(all_pages[page])
+        pdf_out = open(f"{file_name}-page-{page}-out.pdf", "wb")
+        pdf_writer.write(pdf_out)
+        pdf_out.close()
 
-    def extract_array(self, new_pages):
+    def extract_array(self, all_pages, new_pages):
         file_name = path.basename(self.pdf_path).replace(".pdf", "")
         file_dir = path.dirname(self.pdf_path)
-        pdf_writer = fitz.open()
-        for page in new_pages:
-            pdf_writer.insert_pdf(self.pdf_document, from_page=page, to_page=page)
-        pdf_writer.save(file_dir + f"/{file_name}-{len(new_pages)}-pages-out.pdf")
-        pdf_writer.close()
-
-    def close_document(self):
-        self.pdf_document.close()
+        pdf_writer = PdfFileWriter()
+        for i in new_pages:
+            pdf_writer.addPage(all_pages[i])
+        pdf_out = open(file_dir + f"/{file_name}-{len(new_pages)}-pages-out.pdf", "wb")
+        pdf_writer.write(pdf_out)
+        pdf_out.close()
